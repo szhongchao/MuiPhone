5320647fe9e809cdcb264ab40ed54ce06d786778
szc <572233090@qq.com>
2017-04-18 13:17:31 +0800

    version 0.1

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@
 	 **/
 	var baseUrl = 'http://192.168.2.11:8080/busbikeproject/';
 	/*var baseUrl = 'http://busbike.imwork.net:23203/busbikeproject/';*/
+	/*var baseUrl = 'http://138.128.205.72:8080/busbikeproject/';*/
 	owner.login = function(loginInfo, callback) {
 		
 		callback = callback || $.noop;
--- js/common-base.js
+++ js/common-base.js
@@ -1,2 +1,3 @@
 var baseUrl = 'http://192.168.2.11:8080/busbikeproject/';
 /*var baseUrl = 'http://busbike.imwork.net:23203/busbikeproject/';*/
+/*var baseUrl = 'http://138.128.205.72:8080/busbikeproject/';*/
