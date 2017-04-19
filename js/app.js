e8d87a5a5338d201d9e6f19965561bcecf57e398
szc <572233090@qq.com>
2017-04-19 14:31:40 +0800

    version 0.1

--- js/app.js
+++ js/app.js
@@ -7,7 +7,8 @@
 	/**
 	 * 用户登录
 	 **/
-	var baseUrl = 'http://192.168.2.11:8080/busbikeproject/';
+	/*var baseUrl = 'http://192.168.2.11:8080/busbikeproject/';*/
+	var baseUrl = 'http://115.159.75.102:8080/busbikeproject/';
 	/*var baseUrl = 'http://busbike.imwork.net:23203/busbikeproject/';*/
 	/*var baseUrl = 'http://138.128.205.72:8080/busbikeproject/';*/
 	owner.login = function(loginInfo, callback) {
--- js/common-base.js
+++ js/common-base.js
@@ -1,3 +1,4 @@
-var baseUrl = 'http://192.168.2.11:8080/busbikeproject/';
+/*var baseUrl = 'http://192.168.2.11:8080/busbikeproject/';*/
+var baseUrl = 'http://115.159.75.102:8080/busbikeproject/';
 /*var baseUrl = 'http://busbike.imwork.net:23203/busbikeproject/';*/
 /*var baseUrl = 'http://138.128.205.72:8080/busbikeproject/';*/
